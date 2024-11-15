d8f56b94ccd035aee9ee1cd9ad31be9b4070002f
Devanshu Singh <110691532+devanshu-singhh@users.noreply.github.com>
2024-11-15 21:58:26 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,27 +15,27 @@ dotenv.config();
 const PORT=process.env.PORT || 4000;
 const URI=process.env.MongoDBURI;
 
-//connect to mongoDB
-// try{
-//   mongoose.connect(URI,{
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-//   });
-//   console.log("Connected to MongoDB");
-// }catch(error){
-//   console.log("Error: ",error)
+connect to mongoDB
+try{
+  mongoose.connect(URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+  });
+  console.log("Connected to MongoDB");
+}catch(error){
+  console.log("Error: ",error)
 
-// }
+}
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(URI, {});
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        process.exit(1);
-    }
-};
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(URI, {});
+//         console.log("MongoDB Connected");
+//     } catch (error) {
+//         console.error("MongoDB connection error:", error.message);
+//         process.exit(1);
+//     }
+// };
 
 
 //defining route 
